4ba78cc7c09dd5f2ca1e6b162a6922194232cc55
Oscar Trejo <104043537+Ozzythegrouch@users.noreply.github.com>
2022-09-01 18:19:23 -0700

    resolve merge conflicts

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require('express') //gives access to packages for server
 
-const server = express()
+const server = express() //executes the export into server, gives access to methods and properties
+
+server.set('view engine', 'ejs') //set ejs engine
+
+const navs = ['About']
 
 server.get('/', (req,res) =>{
-    res.json({message:"hello"})
+    res.render('pages', { template:'landing' })
 })
 
 server.listen(8080, () => {
