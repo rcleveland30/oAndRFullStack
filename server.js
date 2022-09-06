11569c9690a5fc853c244064f17df78cadb26527
Ron Cleveland <rcleveland30@gmail.com>
2022-09-05 21:43:43 -0500

    add updates to header and footer

--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const server = express() //executes the export into server, gives access to meth
 
 server.set('view engine', 'ejs') //set ejs engine
 
+server.use('/css', express.static(__dirname + "/css"))
+
 const navs = ['About']
 
 server.get('/', (req,res) =>{
     res.render('pages', { template:'landing' })
+    // res.json({message: 'Hello'})
 })
 
 server.get('/about', (req,res) =>{
     res.render('pages', { template:'about' })
+    // res.json({message: 'Hello'})
 })
 
 server.listen(8080, () => {
