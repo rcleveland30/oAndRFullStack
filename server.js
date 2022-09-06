fdc33339bbe8878647c0bee81e3a87c9b744eafd
Oscar Trejo <104043537+Ozzythegrouch@users.noreply.github.com>
2022-09-05 22:55:41 -0700

    login page, header link  modifications, signup page, server page redirects, navs

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const express = require('express') //gives access to packages for server
 
 const server = express() //executes the export into server, gives access to methods and properties
 
+server.use(express.json()) // middleware executes request before server
+server.use('/css', express.static(__dirname + "/css"))
+server.use('/js', express.static(__dirname + "/js"))
 server.set('view engine', 'ejs') //set ejs engine
 
-const navs = ['About']
+const navs = ['About','Contact','SignUp']
 
-server.get('/', (req,res) =>{
-    res.render('pages', { template:'landing' })
+server.get('/', (req,res) => {
+    res.render('pages', { template: 'landing',navs })
 })
 
-server.get('/about', (req,res) =>{
-    res.render('pages', { template:'about' })
+server.get('/About', (req,res) => {
+    res.render('pages', { template: 'about',navs })
+})
+
+server.get('/Contact', (req,res) => {
+    res.render('pages', { template: 'contact',navs })
+})
+
+server.get('/SignUp', (req,res) => {
+    res.render('pages', { template: 'signup',navs })
 })
 
 server.listen(8080, () => {
