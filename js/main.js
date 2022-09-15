d345575afbfc4ce3a67ac6447f1eed70722fc333
Ron Cleveland <rcleveland30@gmail.com>
2022-09-14 22:27:12 -0500

    updates to submit area

--- js/main.js
+++ js/main.js
@@ -4,9 +4,7 @@ loginModal = document.getElementById('loginModal'), //modal element
 modalButton = document.getElementById('modalButton'), // open modal
 closeButton = document.getElementsByClassName('closeButton')[0], //close modal
 contentDisplay = document.querySelector('.page-contain'),
-gamesList = document.querySelector('#games-list'),
-searchBtn = document.queryCommandIndeterm('.search.btn'),
-subRev = document.querySelector('#submit-review')
+searchBtn = document.getElementById('search-btn');
 
 function buildListItem(content) {
     const li = document.createElement('li');
@@ -18,70 +16,84 @@ function buildListItem(content) {
 
 // Landing page information
 const gameList = async() => {
-    const apiData = await fetch('https://api.rawg.io/api/games?key=95016841632347e98a246750ba9e3d58')
-    const jsonData = await apiData.json()
-    const games = jsonData.results
-
-    for (let game of games) {
-        const listItem = buildListItem(game)
-        gamesList.append(listItem)
+    if (window.location.pathname === "/home") {
+        const gamesList = document.querySelector('#games-list');
+        const apiData = await fetch('https://api.rawg.io/api/games?key=95016841632347e98a246750ba9e3d58')
+        const jsonData = await apiData.json()
+        const games = jsonData.results
+    
+        for (let game of games) {
+            const listItem = buildListItem(game)
+            gamesList.append(listItem)
+        }
     }
 }
 gameList()
 
-//modal click for open
-modalButton.addEventListener('click', openModal)
+// //modal click for open
+// modalButton.addEventListener('click', openModal)
 
-//modal click for close
-closeButton.addEventListener('click', closeModal)
+// //modal click for close
+// closeButton.addEventListener('click', closeModal)
 
-//outside click
-window.addEventListener('click', clickOutside)
+// //outside click
+// window.addEventListener('click', clickOutside)
 
-// open modal function
-function openModal(){
-    loginModal.style.display = 'block'
+// // open modal function
+// function openModal(){
+//     loginModal.style.display = 'block'
+// }
+// // close modal function
+// function closeModal(){
+//     loginModal.style.display = 'none'
+// }
+
+// function clickOutside(e){
+//     if(e.target == loginModal){
+//         loginModal.style.display = 'none'
+//     }
+// }
+
+const handleSubmit = e => {
+        e.preventDefault()
+        const data = new FormData(e.target)
+        const stringified = stringifyFormData(data)
+        sendHttpData(stringified)
+};
+
+async function sendHttpData (body) {
+    const gameId = document.getElementById('review').getAttribute('data-gameId')
+    const response = await fetch(`/review/${gameId}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body
+    })
+    const data = await response.json()
+    console.log(data)
 }
-// close modal function
-function closeModal(){
-    loginModal.style.display = 'none'
-}
-
-function clickOutside(e){
-    if(e.target == loginModal){
-        loginModal.style.display = 'none'
-    }
-}
-
-// const handleSubmit = e => {
-//     console.log('he')
-//         e.preventDefault()
-//         const data = new FormData(e.target)
-//         const stringified = stringifyFormData(data)
-//         console.log(stringified)
-//         const userInput = JSON.parse(stringified)
-
-// };
 
 //     logIn.addEventListener('submit', handleSubmit);
 
-// function stringifyFormData(fd) {
-//     const data = {}
-//     for (let key of fd.keys()) {
-//         if (data[key]) {
-//             data[key] = fd.getAll(key)
-//         } else {
-//             data[key] = fd.get(key)
-//         }
-//     }
-//     return JSON.stringify(data, null, 4)
-// }
+function stringifyFormData(fd) {
+    const data = {};
+    for (let key of fd.keys()) {
+        data[key] = fd.get(key);
+    }
+    return JSON.stringify(data, null, 4);
+}
 
+//Review Submission
+if (window.location.pathname.indexOf('games/') > -1) {
+    const reviewForm = document.getElementById('form')
+    reviewForm.addEventListener('submit', handleSubmit)
+}
+
+// //Search button
 // searchBtn.addEventListener ('click', (e) => {
-//     function getInputValue(){
-//         // Selecting the input element and get its value 
-//         var inputVal = document.getElementById("submit-review").value;
-        
-//         // Displaying the value
-//         alert(inputVal);
+//     searchRes = e.target.value
 // })
+
+
+
---
+++ migrations/20220915013712-create-review.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      gameId: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.STRING
+      },
+      review: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reviews');
+  }
+};
